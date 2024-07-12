5aca42370925f83f3f663385ed8118e1a43d9870
workroomprds <14835971-workroomprds@users.noreply.replit.com>
2024-07-12 12:23:16 +0000

    realised return should be string

--- tests/test_base15.py
+++ tests/test_base15.py
@@ -9,6 +9,5 @@ def test_availableCalls():
 	assert callable(getattr(subject, "convertDecimalToBase15", None))
 
 def test_convertDecimalToBase15():
-	assert subject.convertDecimalToBase15(1) == 1
-	assert subject.convertDecimalToBase15(0) == 0
-	assert subject.convertDecimalToBase15(1) == 1
+	assert subject.convertDecimalToBase15(1) == "1"
+	assert subject.convertDecimalToBase15(0) == "0"
