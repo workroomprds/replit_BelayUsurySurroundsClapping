1797837877b04037338c3de25c02343befb955d5
workroomprds <jdl@workroom-productions.com>
2024-07-12 13:03:42 +0000

    Adding longer number after decimal point

--- tests/test_base15.py
+++ tests/test_base15.py
@@ -18,5 +18,8 @@ def test_convertDecimalToBase15():
 	assert subject.convertDecimalToBase15(150) == "A0"
 	assert subject.convertDecimalToBase15(-1) == "-1"
 	assert subject.convertDecimalToBase15(-225) == "-100"
+
+
 assert subject.convertDecimalToBase15(15.6) == "10.9"
 assert subject.convertDecimalToBase15(14.8) == "E.C"
+assert subject.convertDecimalToBase15(14.888) == "E.D4C"
