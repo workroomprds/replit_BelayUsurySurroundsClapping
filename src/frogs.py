ce865244ae72756a2ea542117133aa9681ebea1b
workroomprds <jdl@workroom-productions.com>
2024-09-21 11:38:19 +0000

    AI generated changes to ./src/frogs.py to pass tests in ./tests/test_frogs.py

--- src/frogs.py
+++ src/frogs.py
@@ -5,8 +5,17 @@ class Frogs:
     def __init__(self):
         self.frogs = []
 
+    def newFrog(self, frog):
+        self.frogs.append(frog)
+
     def getFrogs(self):
         return self.frogs
 
-    def newFrog(self, frog):
-        self.frogs.append(frog)
+    def countFrogs(self):
+        return len(self.frogs)
+
+    def findFrog(self, name):
+        for frog in self.frogs:
+            if frog.get('name') == name:
+                return frog
+        return None
--- tests/test_frogs.py
+++ tests/test_frogs.py
@@ -2,18 +2,31 @@
 import unittest
 from src import frogs
 
-subject = frogs.Frogs()
+def setup_function(function):
+	subject = frogs.Frogs()
 
 
 def test_availableCalls():
+	subject = frogs.Frogs()
 	assert callable(getattr(subject, "newFrog", None))
 	assert callable(getattr(subject, "getFrogs", None))
+	assert callable(getattr(subject, "countFrogs", None))
+	assert callable(getattr(subject, "findFrog", None))
 
 def test_getFrogs():
+	subject = frogs.Frogs()
 	assert subject.getFrogs() == []
 
 def test_newFrog():
+	subject = frogs.Frogs()
 	assert subject.getFrogs() == []
 	testFrog = {"name": "frog1"}
 	subject.newFrog(testFrog)
 	assert len(subject.getFrogs()) == 1
+
+def test_countFrogs():
+	subject = frogs.Frogs()
+	assert subject.getFrogs() == []
+	testFrog = {"name": "frog1"}
+	subject.newFrog(testFrog)
+	assert subject.countFrogs() == 1
