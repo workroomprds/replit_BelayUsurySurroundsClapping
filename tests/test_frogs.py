7dc2e6f6eb41f5b0bf8345a8090df852b912439f
workroomprds <jdl@workroom-productions.com>
2024-09-21 09:41:18 +0000

    AI generated changes to ./src/frogs.py to pass tests in ./tests/test_frogs.py

---
+++ tests/test_frogs.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import unittest
+from src import frogs
+
+subject = frogs.Frogs()
+
+
+def test_availableCalls():
+	assert callable(getattr(subject, "newFrog", None))
+
+
+#def test_newThing():
+	#assert subject.newThing() == "nothing supplied"
+	#assert subject.newThing(1) == "1"
+	#assert subject.newThing(2) == "4"
+	#assert subject.newThing(3) == "9"
+	#assert subject.newThing(4) == "16"
+	#assert subject.newThing("bob") == "bob"
+	#assert subject.newThing("Jim") == "Gentleman Jim"
+	#assert subject.newThing(["stop", "start"]) == "you must stop before you can start"
+	#assert subject.newThing(["give", "take"]) == "you must give before you can take"
+	#assert subject.newThing(["1", "2"]) == "you must 1 before you can 2"
