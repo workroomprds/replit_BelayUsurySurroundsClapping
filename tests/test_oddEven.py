a99593c71e4b95483a98a6c30d015a7f3f6980bb
workroomprds <jdl@workroom-productions.com>
2024-11-11 23:16:17 +0000

    AI generated changes to ./src/oddEven.py to pass tests in ./tests/test_oddEven.py

--- tests/test_oddEven.py
+++ tests/test_oddEven.py
@@ -9,7 +9,13 @@ odd = [
 def test_oddEven(candidate):
 	assert "odd" == oddEven(candidate)
 
+even = [
+	22, 18, 400
+]
 
+@pytest.mark.parametrize("candidate", even)
+def test_oddEven(candidate):
+	assert "even" == oddEven(candidate)
 
 def test_oddEven_bad_method():
 	with pytest.raises(ValueError):
