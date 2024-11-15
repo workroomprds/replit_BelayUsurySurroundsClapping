8c1a235cb05249119025bf9d70143efae3115afa
workroomprds <jdl@workroom-productions.com>
2024-11-15 00:22:00 +0000

    get name right for gpt-4o-mini

--- checkOpenAIAccessible.sh
+++ checkOpenAIAccessible.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # gets a response from GPT 4o, times it, extracts the elapsed time only
-{ time llm -t knockknock -m 4o ""; } 2>&1 | awk '/real/{print $2}'
+{ time llm -t knockknock -m gpt-4o-mini ""; } 2>&1 | awk '/real/{print $2}'
